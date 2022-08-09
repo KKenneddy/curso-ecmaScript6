fd7dccb9754f03c488fa0302c45e3c09bf757ab0
Kennedy <nofuequequise@gmail.com>
2022-08-09 19:25:27 -0400

    Cap4

--- src/es6/index.js
+++ src/es6/index.js
@@ -58,3 +58,49 @@ console.log(globalVar);
 const a = 'b';
 a = 'a';
 console.log(a);
+
+let name = 'Kennedy';
+let age = 24;
+
+let obj = {name: name, age: age};
+//es6
+let obj2 = {name, age}
+console.log(obj2);
+
+const names = [
+    {name: 'Kennedy', age: 24},
+    {name: 'Yesica', age: 27}
+];
+
+let listOfNames = names.map(function (item){
+    console.log(item.name);
+});
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age, country)=>{
+    ...
+}
+
+const listOfNames4 = name => {
+    ...
+}
+
+const square = num => num * num;
+
+const helloPromise = () => {
+    return new Promise ((resolve, reject) => {
+        if(false){
+            resolve('hey!');
+        }else{
+            reject('Ups!')
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(()=> console.log('hola'))
+    .catch((err) => {
+        console.log(err)
+    });
