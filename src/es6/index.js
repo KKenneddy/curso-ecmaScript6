1f1ab1c2d518a99b841748c35d6c31bf5f37a52d
Kennedy <nofuequequise@gmail.com>
2022-08-09 18:55:38 -0400

    Cap3

--- src/es6/index.js
+++ src/es6/index.js
@@ -19,3 +19,42 @@ let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem = "Qui consequatur. commodi. Ipsum vel duis yet minima \n" + "otra frase epica que necesitamos.";
+//es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    name: 'Kennedy',
+    age: 24,
+    country: 'RD'
+}
+
+console.log(person.name, person.age);
+
+let {name} = person;
+console.log(name);
+
+let team1 = ['Kennedy', 'Julian', 'Richardo'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+
+let education = ['david', ...team1, ...team2];
+console.log(education);
+
+{
+    var globalVar = 'global var'
+}
+{
+    let globalLet = 'global let'
+    console.log(globalLet);
+}
+console.log(globalVar);
+
+const a = 'b';
+a = 'a';
+console.log(a);
