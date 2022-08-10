398dd6612bccdd10d9354b943151e0561a056ef4
Kennedy <nofuequequise@gmail.com>
2022-08-10 09:23:25 -0400

    Cap12

--- src/es11/index.js
+++ src/es11/index.js
@@ -4,3 +4,33 @@ button.addEventListener("click", async function(){
     const module = await import("./file.js");
     module.hello();
 });
+
+
+const aBigNumber = 9007199254740991n;
+const anotherBigNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+const promise1 = new Promise((resolve, reject)=> reject("Reject"));
+const promise2 = new Promise((resolve, reject)=> resolve("Resolve"));
+const promise3 = new Promise((resolve, reject)=> resolve("Resolve 1"));
+
+
+Promise.allSettled([promise1,promise2,promise3])
+    .then(response => console.log(response))
+    
+
+console.log(window);
+console.log(globalThis);
+
+const fooo = 'ads' ?? 'default string';
+console.log(fooo);
+
+const user = {};
+console.log(user?.profile?.email);
+
+(user?.profile?.email) 
+    ? console.log('email') 
+    : console.log('fail')
+
